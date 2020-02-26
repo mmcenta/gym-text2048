ab67c86d5bd3ea0d731714850dababb9338092cc
Matheus Centa <matheuscenta@gmail.com>
2020-02-26 13:46:21 +0100

    Fix bugs in _compress and _add_random_tile

--- gym_text2048/envs/text2048_env.py
+++ gym_text2048/envs/text2048_env.py
@@ -49,41 +49,42 @@ class Text2048Env(gym.Env):
     def _add_random_tile(self):
         empty_tiles = [t for t in product(*tee(range(self.size)))
                       if self.board[t] == 0]
-        k = self.np_random.randint(len(empty_tiles))
-        value = 1 if self.np_random.random() < 0.9 else 2
-        self.board[empty_tiles[k]] = value
+        if len(empty_tiles) > 0:
+            k = self.np_random.randint(len(empty_tiles))
+            value = 1 if self.np_random.random() < 0.9 else 2
+            self.board[empty_tiles[k]] = value
 
     def _compress(self, view):
         changed = False
-        for col in range(self.size):
-            top, bottom = 0, self.size - 1
-            while top < bottom:
-                if view[top][col] != 0:
-                    top += 1
-                elif view[bottom][col] == 0:
-                    bottom -= 1
-                else:
-                    view[top][col], view[bottom][col] = view[bottom][col], view[top][col]
-                    top, bottom = top + 1, bottom - 1
-                    changed = True
+        for j in range(self.size):
+            count = 0
+            for i in range(self.size):
+                if view[i][j] != 0:
+                    view[count][j], view[i][j] = view[i][j], view[count][j]
+                    if count != j:
+                        changed = True
+                    count += 1
         return changed
 
     def _merge(self, view):
         reward = 0
-        for col in range(self.size):
+        for j in range(self.size):
             for i in range(self.size - 1):
-                if view[i][col] == view[i + 1][col] and view[i][col] != 0:
-                    view[i][col] += 1
-                    view[i + 1][col] = 0
-                    reward += (2 ** view[i][col])
+                if view[i][j] == view[i + 1][j] and view[i][j] != 0:
+                    view[i][j] += 1
+                    view[i + 1][j] = 0
+                    reward += (2 ** view[i][j])
         return reward
 
     def _is_done(self):
+        def can_merge(i, j):
+            return any([self.board[i][j] == self.board[i + di][j + dj]
+                        for di, dj in ((-1, 0), (0, -1), (0, 1), (1, 0))
+                        if (i + di < self.size and i + di >= 0) and
+                           (j + dj < self.size and j + dj >= 0)])
+
         for i, j in product(*tee(range(self.size))):
-            if (self.board[i][j] == 0 or
-                any([self.board[i][j] == self.board[i + di][j + dj]
-                     for (di, dj) in ((-1, 0), (0, -1), (0, 1), (1, 0))
-                     if i + di < self.size and j + dj < self.size])):
+            if self.board[i][j] == 0 or can_merge(i, j):
                 return False
         return True
 
@@ -91,13 +92,13 @@ class Text2048Env(gym.Env):
         assert self.action_space.contains(action)
 
         view = np.rot90(self.board, k=action)
-        changed = self.compress(view)
+        changed = self._compress(view)
         reward = self._merge(view)
-        changed = (changed or reward > 0)
-        if changed:
+        if changed or reward > 0:
             self._compress(view)
             self._add_random_tile()
 
+        self.lastaction = action
         self.score += reward
         done = self._is_done()
 
@@ -105,15 +106,17 @@ class Text2048Env(gym.Env):
 
     def reset(self):
         self.score = 0
+        self.lastaction = None
         self.board = np.zeros((self.size, self.size), dtype=np.int8)
         self._add_random_tile()
         self._add_random_tile()
+        return np.ravel(self.board)
 
     def render(self, mode='human'):
         out = StringIO if mode == 'ansi' else sys.stdout
 
-        scoreline = "\n SCORE: " + str(self.score) + "\n"
-        hline = "\n|" + "|".join(["-----"] * len(board)) + "|\n"
+        if self.lastaction is not None:
+            out.write("  ({})\n".format(["Up", "Right", "Down", "Left"][self.lastaction]))
 
         def tile_to_symbol(tile):
             if tile == 0:
@@ -122,9 +125,9 @@ class Text2048Env(gym.Env):
                 return colorize("{:>5}".format(2 ** tile), *TILE_FORMAT[tile])
             return colorize("{:>5}".format(2 ** tile), "gray", False)
 
-        symbols = [[tile_to_symbol(t) for t in line] for line in board]
-        out.write(scoreline +
-                  hline +
+        hline = "\n|" + "|".join(["-----"] * len(self.board)) + "|\n"
+        symbols = [[tile_to_symbol(t) for t in line] for line in self.board]
+        out.write(hline +
                   hline.join('|' + '|'.join(line) + '|' for line in symbols) +
                   hline)
 

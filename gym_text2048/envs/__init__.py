1d983fafe1be0685a818eec5af489b7dd9670b60
Matheus Centa <matheuscenta@gmail.com>
2020-03-01 19:38:31 +0100

    Import all envs on __init__.py

--- gym_text2048/envs/__init__.py
+++ gym_text2048/envs/__init__.py
@@ -1,1 +1,4 @@
 from gym_text2048.envs.text2048_env import Text2048Env
+from gym_text2048.envs.text2048_capped import Text2048CappedEnv
+from gym_text2048.envs.text2048_withheuristic_env import Text2048WithHeuristicEnv
+from gym_text2048.envs.text2048_capped_withheuristic import Text2048CappedWithHeuristicEnv
